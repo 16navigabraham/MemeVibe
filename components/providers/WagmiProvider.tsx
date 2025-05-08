82ec20583dd0e4eaafb4891d1e615d7a18ce7eb9
Adebanjo Abraham.I <navigatorabraham17@gmail.com>
2025-05-08 12:14:11 +0100

    wallet and frame setup

--- components/providers/WagmiProvider.tsx
+++ components/providers/WagmiProvider.tsx
@@ -1,18 +1,16 @@
 import { createConfig, http, WagmiProvider } from "wagmi";
-import { base, degen, mainnet, optimism, unichain } from "wagmi/chains";
+import { base } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
+import { frameConnector } from "../../lib/connector";
+
+
 
 export const config = createConfig({
-  chains: [base, optimism, mainnet, degen, unichain],
+  chains: [base],
   transports: {
     [base.id]: http(),
-    [optimism.id]: http(),
-    [mainnet.id]: http(),
-    [degen.id]: http(),
-    [unichain.id]: http(),
   },
-  connectors: [farcasterFrame()],
+  connectors: [frameConnector()],
 });
 
 const queryClient = new QueryClient();
