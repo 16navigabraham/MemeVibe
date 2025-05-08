f7c425173cc5a9bf1dc648815954e1afc18ec1b1
Adebanjo Abraham.I <navigatorabraham17@gmail.com>
2025-05-08 14:33:32 +0100

    page.tsx upadte

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
+
 import dynamic from "next/dynamic";
+// import Layout from "./layout.jsx";
 
 
-const Demo = dynamic(() => import("../components/Demo"), {
+const Layout = dynamic(() => import("./layout"), {
   ssr: false,
 });
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col p-4">
-      <Demo />
+      
+      <Layout/>
     </main>
   );
 }
