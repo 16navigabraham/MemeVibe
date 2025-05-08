bab6535a3a68bccc8eb4ee5a4b004de8fbbed3d1
Adebanjo Abraham.I <navigatorabraham17@gmail.com>
2025-05-08 15:27:52 +0100

    updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { Navbar } from "../components/navbar";
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <Navbar />
       <HeroSection />
       <MemeGallery />
       {/* { Welcome to the Meme Cast! } */}
